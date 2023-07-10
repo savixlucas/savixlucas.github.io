d33ee3f261435e4478b1048955dc887148e8784d
evertonsavio <everluca@outlook.com>
2023-07-10 14:48:53 -0300

    savixlucas legacy portfolio

---
+++ precache-manifest.95127dc4c496d143f3c256fc9391f90a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de6901904a166bd97640f6dbf2d02c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca950220a61e881af38b",
+    "url": "/static/css/2.930a5b2e.chunk.css"
+  },
+  {
+    "revision": "7d58108fb2157683dc84",
+    "url": "/static/css/main.2787170b.chunk.css"
+  },
+  {
+    "revision": "ca950220a61e881af38b",
+    "url": "/static/js/2.4cde7f7d.chunk.js"
+  },
+  {
+    "revision": "8e56a5067685421837c8585bb189326c",
+    "url": "/static/js/2.4cde7f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d58108fb2157683dc84",
+    "url": "/static/js/main.18eab718.chunk.js"
+  },
+  {
+    "revision": "72e1b570306afe9981ec",
+    "url": "/static/js/runtime-main.f719501c.js"
+  },
+  {
+    "revision": "7574447d2826c0409734e90f80dffb21",
+    "url": "/static/media/LumniM.7574447d.mp4"
+  },
+  {
+    "revision": "9d76aaa0f90ca1ceac548ce75cbf7e65",
+    "url": "/static/media/fraudes.9d76aaa0.png"
+  },
+  {
+    "revision": "518bb9979cdbe9abd844b5169eedc415",
+    "url": "/static/media/images.518bb997.jpg"
+  },
+  {
+    "revision": "a793b79a9f60e73e8b2a1ac4888a69a5",
+    "url": "/static/media/java.a793b79a.png"
+  },
+  {
+    "revision": "6b0790f1827e703f68104d22ad09f17d",
+    "url": "/static/media/manutencao.6b0790f1.png"
+  },
+  {
+    "revision": "a3bbc65c8729c001d42d0494cfe10091",
+    "url": "/static/media/pc3.a3bbc65c.jpg"
+  },
+  {
+    "revision": "a54db5e3895a4d4e32eb17fe7c43a174",
+    "url": "/static/media/perfilM.a54db5e3.mp4"
+  },
+  {
+    "revision": "45c326d281a08de5b92f34474e51cf71",
+    "url": "/static/media/portfolio.45c326d2.png"
+  },
+  {
+    "revision": "5aeab710ebe8255c5903e6f908c6e641",
+    "url": "/static/media/sonoAPPM.5aeab710.mp4"
+  }
+]);
