a85167ab522be0bfce8b9b02e9d9df57ddf92e5a
evertonsavio <everluca@outlook.com>
2023-11-10 09:23:43 -0300

    update site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d501504adfc39493042f0ffc431783e.js"
+  "/precache-manifest.96bb848e891da217802c66b526f92878.js"
 );
 
 self.addEventListener('message', (event) => {
